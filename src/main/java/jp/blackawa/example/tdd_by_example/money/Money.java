11f950934e0db94bb0fe16cf78fc2087ddb54a89
blackawa <3104kurokawa@gmail.com>
2019-11-21 21:00:08 +0900

    :green_heart: chapter 7

--- src/main/java/jp/blackawa/example/tdd_by_example/money/Money.java
+++ src/main/java/jp/blackawa/example/tdd_by_example/money/Money.java
@@ -5,6 +5,7 @@ public class Money {
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return this.amount == money.amount;
+        return this.amount == money.amount
+                && getClass().equals(money.getClass());
     }
 }
