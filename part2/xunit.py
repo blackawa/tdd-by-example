3cc17b3956902f9b783adb28749ef6f636d44f48
blackawa <3104kurokawa@gmail.com>
2019-11-24 20:34:59 +0900

    :green_heart: chaper 19

--- part2/xunit.py
+++ part2/xunit.py
@@ -1,27 +1,29 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
     def testMethod(self):
         self.wasRun = 1
-
-# test = WasRun("testMethod")
-# print(test.wasRun)
-# test.run()
-# print(test.wasRun)
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert (not test.wasRun)
-        test.run()
-        assert (test.wasRun)
+        self.test.run()
+        assert (self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
